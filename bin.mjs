ef62b5b631296dfd00dd195dbf231f78b486260c
Ryan Ramage <r@mage.rocks>
2025-02-25 16:48:06 -0700

    feat: Add bin.mjs script for CLI functionality

---
+++ bin.mjs
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { BreakoutRoom } from "@agree-able/room";
+import { z } from "zod";
+
+// Create an MCP server
+const server = new McpServer({
+  name: "Join a room with another agent",
+  version: "1.0.0"
+});
+
+const rooms = {}
+const messagesByRoom = {}
+
+server.tool(
+  'join-with-invite',
+  'join a room with an invite code',
+  { invite: z.string() },
+  ({ invite }) => new Promise(async (resolve, reject) => {
+    const room = new BreakoutRoom({ invite })
+    await room.ready()
+    room.on("message", async (message) => {
+      if (!message.data) return
+      messagesByRoom[room.roomId].push(message)
+      // if this is not the first message, just return
+      if (messagesByRoom[room.roomId].length > 1) return
+      // otherwise lets resolve the promise with the message
+      let response = `Room created with id: ${roomInfo.roomId}. Host first message to us is: ${message.data}`
+      resolve({
+        content: [{ type: 'text', text: response }]
+      })
+    })
+    room.on("peerLeft", (key) => {
+    });
+    const roomInfo = room.getRoomInfo()
+    rooms[roomInfo.roomId] = room
+    messagesByRoom[roomInfo.roomId] = []
+  })
+);
+
+server.tool(
+  'send-message',
+  'send a message to a room',
+  { roomId: z.string(), message: z.string() },
+  async ({ roomId, message }) =>  {
+    const room = rooms[roomId]
+    if (!room) {
+      reject(`Room with id ${roomId} not found`)
+    }
+    await room.message(message)
+    return {
+      content: [{ type: 'text', text: `Message sent to room ${roomId}` }]
+    }
+  })
+
+// // Add a dynamic greeting resource
+// server.resource(
+//   "greeting",
+//   new ResourceTemplate("greeting://{name}", { list: undefined }),
+//   async (uri, { name }) => ({
+//     contents: [{
+//       uri: uri.href,
+//       text: `Hello, ${name}!`
+//     }]
+//   })
+// );
+
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+await server.connect(transport);
